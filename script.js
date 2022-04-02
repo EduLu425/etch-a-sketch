0b954c59303c52e8c565f5fa277eb27f3bffda48
Eduardo <ed.luna80@gmail.com>
2022-04-01 19:27:47 -0700

    Add basic functionality to etch-a-sketch
    Set up basic CSS flex structure. Program javascript to generate 16 x 16
    grid of divs. Add event handler to each div with queryselector and for
    of loop.

--- script.js
+++ script.js
@@ -1,6 +1,25 @@
 const container = document.querySelector('.container');
-console.log(container);
+const row = document.querySelector('.row');
+const column = document.querySelector('.column');
+
+function createGrid() {
+    for (i = 0; i < 16; i++) {
+        row.appendChild(column.cloneNode(true));
+}
+
+    for (i = 0; i < 15; i++) {
+        container.appendChild(row.cloneNode(true));
+    }
 
 
+}
 
-å
+createGrid();
+
+const newColumns = document.querySelectorAll('.column');
+
+for (const element of newColumns) {
+    element.addEventListener('mouseenter', () => {
+        element.style.backgroundColor = 'black';
+    })
+}
